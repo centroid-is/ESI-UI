d856eaf5ae3989813220d90a242d080b1687e7f1
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-04-19 10:43:22 +0000

    adwatia

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,91 @@
-import 'package:esi_ui/xml_parsing.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:macos_ui/macos_ui.dart';
+import 'dart:async';
 
-void main() async {
-  runApp(const EsiUi());
+import 'package:adwaita/adwaita.dart';
+import 'package:esi_ui/xml_parsing.dart';
+import 'package:flutter/material.dart';
+import 'package:libadwaita/libadwaita.dart';
+import 'package:libadwaita_window_manager/libadwaita_window_manager.dart';
+import 'package:universal_io/io.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:window_manager/window_manager.dart';
+
+Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  const windowOptions = WindowOptions(
+    size: Size(1000, 600),
+    minimumSize: Size(400, 450),
+    skipTaskbar: false,
+    backgroundColor: Colors.transparent,
+    titleBarStyle: TitleBarStyle.hidden,
+    title: 'Libadwaita Example',
+  );
+
+  unawaited(
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      if (Platform.isLinux || Platform.isMacOS) {
+        await windowManager.setAsFrameless();
+      }
+      await windowManager.show();
+      await windowManager.focus();
+    }),
+  );
+  runApp(MyApp());
 }
 
-class EsiUi extends StatelessWidget {
-  const EsiUi({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.system);
+
   @override
   Widget build(BuildContext context) {
-    return MacosApp(
-      title: 'ESI-UI',
-      theme: MacosThemeData.dark(),
-      home: const MainPage(),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeNotifier,
+      builder: (_, ThemeMode currentMode, __) {
+        return MaterialApp(
+          builder: (context, child) {
+            final virtualWindowFrame = VirtualWindowFrameInit();
+
+            return virtualWindowFrame(context, child);
+          },
+          theme: AdwaitaThemeData.light(),
+          darkTheme: AdwaitaThemeData.dark(),
+          debugShowCheckedModeBanner: false,
+          home: MainPage(themeNotifier: themeNotifier),
+          themeMode: currentMode,
+        );
+      },
     );
   }
 }
 
-class object_display_page extends StatelessWidget {
-  final EsiFile? selectedFile;
-  const object_display_page({super.key, this.selectedFile});
-
-  @override
-  Widget build(BuildContext context) {
-    if (selectedFile == null) return const Text('No file selected');
-    return Container(color: MacosColors.appleMagenta);
-    // return ListView.builder(itemBuilder: (context, count) {
-    //   debugPrint(count.toString());
-    //   return Container(color: MacosColors.appleCyan);
-    // });
-  }
-}
-
 class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+  const MainPage({super.key, required this.themeNotifier});
+
+  final ValueNotifier<ThemeMode> themeNotifier;
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
-  int pageIndex = 0;
+  ValueNotifier<int> counter = ValueNotifier<int>(0);
+
+  late ScrollController listController;
+  late ScrollController settingsController;
+  late FlapController _flapController;
+  int? _currentIndex = 0;
+
   EsiFile? selectedFile;
-  final _controller = MacosTabController(
-    initialIndex: 0,
-    length: 3,
-  );
   // Parsed file list
   @override
   void initState() {
     super.initState();
+    listController = ScrollController();
+    settingsController = ScrollController();
+    _flapController = FlapController();
+    _flapController.addListener(() => setState(() {}));
     // TODO: Do this inside a builder or something.
     // TODO: Add add file functionality with a file picker
     parseEsiFile('test/test_files/Beckhoff EL4xxx.xml').then((value) => setState(() {
@@ -60,90 +93,201 @@ class _MainPageState extends State<MainPage> {
         }));
   }
 
+  @override
+  void dispose() {
+    listController.dispose();
+    settingsController.dispose();
+    super.dispose();
+  }
+
+  void changeTheme() => widget.themeNotifier.value = widget.themeNotifier.value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+
   Map<String, EsiFile> files = {};
   @override
   Widget build(BuildContext context) {
-    return PlatformMenuBar(
-      menus: const [],
-      child: MacosWindow(
-        sidebar: Sidebar(
-            top: MacosSearchField(
-              placeholder: 'Search',
-              onResultSelected: (result) {
-                debugPrint('searched for $result');
-              },
-              results: const [
-                // Could use this to filter files when we have allot of them
-                SearchResultItem('Buttons'),
-                SearchResultItem('Indicators'),
-                SearchResultItem('Fields'),
-                SearchResultItem('Colors'),
-                SearchResultItem('Dialogs and Sheets'),
-                SearchResultItem('Toolbar'),
-                SearchResultItem('ResizablePane'),
-                SearchResultItem('Selectors'),
-              ],
-            ),
-            minWidth: 200,
-            builder: (context, scrollController) {
-              return SidebarItems(
-                  currentIndex: pageIndex,
-                  onChanged: (i) {
-                    setState(() {
-                      selectedFile = files.values.elementAt(i);
-                      pageIndex = i;
-                    });
-                  },
-                  scrollController: scrollController,
-                  itemSize: SidebarItemSize.medium,
-                  items: files.values.map((e) => SidebarItem(leading: const MacosIcon(CupertinoIcons.square_on_square), label: Text(e.name))).toList());
-            },
-            bottom: MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: GestureDetector(
-                    child: const MacosListTile(
-                      leading: MacosIcon(CupertinoIcons.add),
-                      title: Text('Add File'),
-                      subtitle: Text('Add more files to ESI-UI'),
-                    ),
-                    onTap: () async {
-                      //TODO: Look into other file picker libs. Needs some binary named zenity
-                      FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ['xml']);
-                      if (result == null) {
-                        return;
-                      }
-                      for (var file in result.files) {
-                        if (file.path == null) {
-                          continue;
-                        }
-                        try {
-                          final EsiFile parsedFile = await parseEsiFile(file.path!);
-                          setState(() {
-                            files.addAll({parsedFile.name: parsedFile});
-                          });
-                        } catch (e) {
-                          debugPrint(e.toString());
-                        }
-                      }
-                    }))),
-        endSidebar: Sidebar(
-          startWidth: 200,
-          minWidth: 200,
-          maxWidth: 300,
-          shownByDefault: false,
-          builder: (context, _) {
-            return Center(
-                child: Container(
-              color: MacosColors.appleCyan,
-              child: const Text('End Sidebar'),
-            ));
-          },
+    final developers = {'Someones name': 'username'};
+    return AdwScaffold(
+      flapController: _flapController,
+      actions: AdwActions().windowManager,
+      start: [
+        AdwHeaderButton(
+          icon: const Icon(Icons.view_sidebar_outlined, size: 19),
+          isActive: _flapController.isOpen,
+          onPressed: () => _flapController.toggle(),
         ),
-        child: MacosTabView(
-            controller: _controller,
-            tabs: const [MacosTab(label: 'Objects'), MacosTab(label: 'Vendor'), MacosTab(label: 'Pdo?')],
-            children: [object_display_page(selectedFile: selectedFile), Container(color: MacosColors.appleBlue), Container(color: MacosColors.appleRed)]),
+        AdwHeaderButton(
+          icon: Icon(widget.themeNotifier.value == ThemeMode.light ? Icons.nightlight_round : Icons.sunny, size: 15),
+          onPressed: changeTheme,
+        ),
+      ],
+      title: const Text('ESI-UI'),
+      end: [
+        GtkPopupMenu(
+          body: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              AdwButton.flat(
+                onPressed: () {
+                  counter.value = 0;
+                  Navigator.of(context).pop();
+                },
+                padding: AdwButton.defaultButtonPadding.copyWith(
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: const Text(
+                  'Reset Counter',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              const Divider(),
+              AdwButton.flat(
+                padding: AdwButton.defaultButtonPadding.copyWith(
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: const Text(
+                  'Preferences',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              AdwButton.flat(
+                padding: AdwButton.defaultButtonPadding.copyWith(
+                  top: 10,
+                  bottom: 10,
+                ),
+                onPressed: () => showDialog<Widget>(
+                  context: context,
+                  builder: (ctx) => AdwAboutWindow(
+                    issueTrackerLink: 'https://github.com/gtk-flutter/libadwaita/issues',
+                    appIcon: const Icon(Icons.run_circle_sharp), //Image.asset('assets/logo.png'),
+                    credits: [
+                      AdwPreferencesGroup.creditsBuilder(
+                        title: 'Developers',
+                        itemCount: developers.length,
+                        itemBuilder: (_, index) => AdwActionRow(
+                          title: developers.keys.elementAt(index),
+                          onActivated: () => launchUrl(
+                            Uri.parse(
+                              'https://github.com/${developers.values.elementAt(index)}',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                    copyright: 'Copyright 2024 Skaginn3x',
+                    license: const Text(
+                      'MIT License, This program comes with no warranty.',
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'About this Demo',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+      flap: (isDrawer) => AdwSidebar(
+        currentIndex: _currentIndex,
+        isDrawer: isDrawer,
+        children: files.values.map((e) => AdwSidebarItem(leading: const Icon(Icons.inbox_sharp), label: e.name)).toList(),
+        onSelected: (index) => setState(() {
+          _flapController.close();
+          _currentIndex = index;
+        }),
       ),
+      body: AdwViewStack(
+        animationDuration: const Duration(milliseconds: 100),
+        index: _currentIndex,
+        children: [
+          Container(color: Colors.amber),
+          Container(color: Colors.red),
+          Container(color: Colors.black),
+        ],
+      ),
+      // child: MacosWindow(
+      //   sidebar: Sidebar(
+      //       top: MacosSearchField(
+      //         placeholder: 'Search',
+      //         onResultSelected: (result) {
+      //           debugPrint('searched for $result');
+      //         },
+      //         results: const [
+      //           // Could use this to filter files when we have allot of them
+      //           SearchResultItem('Buttons'),
+      //           SearchResultItem('Indicators'),
+      //           SearchResultItem('Fields'),
+      //           SearchResultItem('Colors'),
+      //           SearchResultItem('Dialogs and Sheets'),
+      //           SearchResultItem('Toolbar'),
+      //           SearchResultItem('ResizablePane'),
+      //           SearchResultItem('Selectors'),
+      //         ],
+      //       ),
+      //       minWidth: 200,
+      //       builder: (context, scrollController) {
+      //         return SidebarItems(
+      //             currentIndex: pageIndex,
+      //             onChanged: (i) {
+      //               setState(() {
+      //                 selectedFile = files.values.elementAt(i);
+      //                 pageIndex = i;
+      //               });
+      //             },
+      //             scrollController: scrollController,
+      //             itemSize: SidebarItemSize.medium,
+      //             items:
+      //       },
+      //       bottom: MouseRegion(
+      //           cursor: SystemMouseCursors.click,
+      //           child: GestureDetector(
+      //               child: const MacosListTile(
+      //                 leading: MacosIcon(CupertinoIcons.add),
+      //                 title: Text('Add File'),
+      //                 subtitle: Text('Add more files to ESI-UI'),
+      //               ),
+      //               onTap: () async {
+      //                 //TODO: Look into other file picker libs. Needs some binary named zenity
+      //                 FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ['xml']);
+      //                 if (result == null) {
+      //                   return;
+      //                 }
+      //                 for (var file in result.files) {
+      //                   if (file.path == null) {
+      //                     continue;
+      //                   }
+      //                   try {
+      //                     final EsiFile parsedFile = await parseEsiFile(file.path!);
+      //                     setState(() {
+      //                       files.addAll({parsedFile.name: parsedFile});
+      //                     });
+      //                   } catch (e) {
+      //                     debugPrint(e.toString());
+      //                   }
+      //                 }
+      //               }))),
+      //   endSidebar: Sidebar(
+      //     startWidth: 200,
+      //     minWidth: 200,
+      //     maxWidth: 300,
+      //     shownByDefault: false,
+      //     builder: (context, _) {
+      //       return Center(
+      //           child: Container(
+      //         color: MacosColors.appleCyan,
+      //         child: const Text('End Sidebar'),
+      //       ));
+      //     },
+      //   ),
+      //   child: MacosTabView(
+      //       controller: _controller,
+      //       tabs: const [MacosTab(label: 'Objects'), MacosTab(label: 'Vendor'), MacosTab(label: 'Pdo?')],
+      //       children: [object_display_page(selectedFile: selectedFile), Container(color: MacosColors.appleBlue), Container(color: MacosColors.appleRed)]),
+      // ),
     );
   }
 }
